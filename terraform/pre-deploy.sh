676c98f7b160a164700a3477f86016151aa5ad18
Anjali <68515341+peachycloudsecurity@users.noreply.github.com>
2024-12-16 05:03:59 +0530

    Update pre-deploy.sh

--- terraform/pre-deploy.sh
+++ terraform/pre-deploy.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#!/bin/bash
+set -e
+set -x  # Enables debug mode (shows commands as they are executed)
 
 echo "====================================================================="
-echo "    VSCode Pre-deployment Script By peachycloudsecurity                  "
+echo "    Pre-deployment Script By peachycloudsecurity                      "
 echo "====================================================================="
 echo ""
 echo "Initializing the setup of required dependencies."
-echo "Refer to original script for credit at github.com/kubernetesvillage/ecr_eks_security_masterclass_public/."
 echo ""
 echo ""
 
+# Copied from https://github.com/kubernetesvillage/terraform-eks/blob/main/pre-deploy.sh
+
+# Redirect output to a log file
+exec > >(tee -i install.log)
+exec 2>&1
 
 # Function to install AWS CLI
 install_aws() {
     echo "Installing AWS CLI..."
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" || { echo "AWS CLI download failed. Exiting."; exit 1; }
     unzip awscliv2.zip
     sudo ./aws/install
     rm -rf awscliv2.zip aws
@@ -26,7 +31,7 @@ install_aws() {
 install_eksctl() {
     echo "Installing eksctl..."
     PLATFORM=$(uname -s)_amd64
-    curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+    curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz" || { echo "eksctl download failed. Exiting."; exit 1; }
     tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
     sudo mv /tmp/eksctl /usr/local/bin
     echo "eksctl installation complete."
@@ -39,60 +44,83 @@ install_kubectl() {
     case $ARCH in
         x86_64) BIN_ARCH="amd64" ;;
         aarch64) BIN_ARCH="arm64" ;;
-        *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+        *) echo "Unsupported architecture: $ARCH. Exiting."; exit 1 ;;
     esac
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$BIN_ARCH/kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$BIN_ARCH/kubectl" || { echo "kubectl download failed. Exiting."; exit 1; }
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     echo "kubectl installation complete."
 }
 
-# Function to install Terraform 
-install_terraform() {
-    echo "Installing Terraform..."
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        OS=$ID
-    else
-        echo "Cannot determine OS type. Exiting."
-        exit 1
-    fi
+# Function to install OpenTofu
+install_opentofu() {
+    echo "Installing OpenTofu..."
+    sudo yum update -y
+    sudo yum install -y yum-utils uuid jq || { echo "Package installation failed. Exiting."; exit 1; }
 
-    if [[ "$OS" == "ubuntu" ]]; then
-        sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-        echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-        sudo apt-get update && sudo apt-get install -y terraform
+    # Download the installer script
+    curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh || { echo "OpenTofu installer download failed. Exiting."; exit 1; }
 
-    elif [[ "$OS" == "centos" || "$OS" == "rhel" || "$OS" == "fedora" ]]; then
-        sudo yum install -y yum-utils uuid-runtime jq
-        sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-        sudo yum -y install terraform
-    else
-        echo "Unsupported OS: $OS. Please install Terraform manually."
-        exit 1
-    fi
-    echo "Terraform installation complete."
+    # Give execution permissions
+    chmod +x install-opentofu.sh
+
+    # Run the installer using RPM method
+    ./install-opentofu.sh --install-method rpm
+
+    # Clean up the installer script
+    rm -f install-opentofu.sh
+
+    echo "OpenTofu installation complete."
 }
 
-# Function to install jq & other dependencies
-install_jq() {
-    echo "Installing jq..."
-    if [[ "$OS" == "ubuntu" ]]; then
-        sudo apt-get update && sudo apt-get install -y jq uuid-runtime
-    elif [[ "$OS" == "centos" || "$OS" == "rhel" || "$OS" == "fedora" ]]; then
-        sudo yum install -y jq
-    else
-        echo "Unsupported OS: $OS. Please install jq manually."
-        exit 1
-    fi
-    echo "jq installation complete."
+# Function to install jq, envsubst, and bash-completion
+install_utilities() {
+    echo "Installing utilities..."
+    sudo yum -y install jq gettext bash-completion moreutils || { echo "Utilities installation failed. Exiting."; exit 1; }
+    echo "Utilities installation complete."
+}
+
+# Function to install yq
+install_yq() {
+    echo "Installing yq..."
+    yq_version='4.44.3'
+    curl -sLO "https://github.com/mikefarah/yq/releases/download/v$yq_version/yq_linux_amd64" || { echo "yq download failed. Exiting."; exit 1; }
+    chmod +x ./yq_linux_amd64
+    sudo mv ./yq_linux_amd64 /usr/local/bin/yq
+    echo "yq installation complete."
+}
+
+# Function to install Helm
+install_helm() {
+    echo "Installing Helm..."
+    helm_version='3.16.2'
+    curl -sLO "https://get.helm.sh/helm-v$helm_version-linux-amd64.tar.gz" || { echo "Helm download failed. Exiting."; exit 1; }
+    tar zxf helm-v$helm_version-linux-amd64.tar.gz
+    sudo mv ./linux-amd64/helm /usr/local/bin
+    rm -rf linux-amd64/ helm-v$helm_version-linux-amd64.tar.gz
+    echo "Helm installation complete."
+}
+
+# Function to install kubeseal
+install_kubeseal() {
+    echo "Installing kubeseal..."
+    kubeseal_version='0.27.2'
+    curl -sLO "https://github.com/bitnami-labs/sealed-secrets/releases/download/v$kubeseal_version/kubeseal-$kubeseal_version-linux-amd64.tar.gz" || { echo "kubeseal download failed. Exiting."; exit 1; }
+    tar xfz kubeseal-$kubeseal_version-linux-amd64.tar.gz
+    chmod +x kubeseal
+    sudo mv ./kubeseal /usr/local/bin
+    rm kubeseal-$kubeseal_version-linux-amd64.tar.gz
+    echo "kubeseal installation complete."
 }
 
 # Function to check if a binary is installed
 check_binary() {
     if ! command -v $1 &> /dev/null; then
-        echo "$1 could not be found. Installing $1..."
-        install_$1
+        echo "$1 not found. Installing $1..."
+        if [ "$1" = "jq" ] || [ "$1" = "envsubst" ]; then
+            install_utilities
+        else
+            install_$1
+        fi
     else
         echo "$1 is already installed."
     fi
@@ -115,12 +143,21 @@ else
     exit 1
 fi
 
+# OS compatibility check
+if [[ "$OS" != "amzn" && "$OS" != "centos" && "$OS" != "rhel" ]]; then
+    echo "Unsupported OS: $OS. This script supports Amazon Linux, CentOS, or RHEL only."
+    exit 1
+fi
+
 # Check and install required binaries
 echo "Checking and installing required binaries..."
 check_binary aws
 check_binary eksctl
 check_binary kubectl
-check_binary terraform
+check_binary opentofu
 check_binary jq
+check_binary yq
+check_binary helm
+check_binary kubeseal
 
 echo "Pre-deployment checks and installations are complete."
